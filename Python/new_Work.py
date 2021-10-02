9be2f34d6f1bea9339108a2d64c586ec3e7bc154
Harshit Mody <55501384+harshitmody72@users.noreply.github.com>
2021-10-02 20:44:05 +0530

    Initial Commit

---
+++ Python/new_Work.py
@@ -0,0 +1,20 @@
+# s = None
+# for i in [9,8,7]:
+#     if s == i:
+#         s=i
+#
+# print(s)
+
+
+class Person:
+    def math(self):
+        pass
+
+    def ritu(self):
+        pass
+
+
+poin =  Person()
+poin.ritu()
+har = Person()
+har.rit
